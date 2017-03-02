9dfd2ac69af3e68542f02860d96f51bf31fb0592
Moritz <cmhoequist@gmail.com>
2017-03-02 13:13:11 -0600

    First commit

---
+++ js/mech.js
@@ -0,0 +1,38 @@
+var game = new Phaser.Game('100', '100', Phaser.AUTO, '');
+//Recall: p1 & p2 are width and height. Given as strings, they are a percentage value of the browser window.
+//p3: preferred rendering context
+//p4: parent element in the DOM: body if none given
+//p5: state object. This and all following parameters are optional.
+
+var boot = function(game){
+  console.log('what does console.log actually do?');
+}
+
+boot.prototype = {
+  preload : function(){
+  },
+  init: function(){
+    // scale the game 4x
+    game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
+    game.scale.setUserScale(4, 4);
+    // enable crisp rendering
+    game.renderer.renderSession.roundPixels = true;
+    Phaser.Canvas.setImageRenderingCrisp(this.game.canvas)
+  }
+  create : function(){
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    game.scale.pageAlignHorizontally = true;
+    game.scale.setScreenSize(true);
+    game.state.start('Preflight');
+  }
+}
+
+//Game states are like levels: each represents a phase of the game. Recall that PRELOAD, CREATE, UPDATE, and RENDER are the four core functions of each state.
+game.state.add('Boot',boot);
+game.state.add('Preflight', preflight);
+// // game.state.add('Combat');
+// // game.state.add('Docking');
+// // game.state.add('Gameover');
+//
+// //Initialize a state
+game.state.start('Boot');
---
+++ js/preflight.js
@@ -0,0 +1,48 @@
+var hud, blip, radarMech, cursors;
+
+var preflight = function(game){
+}
+
+preflight.prototype = {
+
+
+  //Member functions declared here
+  preload : function(){
+    //Note that radar coloring is #14a10c
+      game.load.image('background','assets/reticule2.png');
+      game.load.image('blip','assets/radarBlip.png');
+      game.load.image('mech','assets/radarCenter.png');
+  },
+  create : function(){
+    //Create HUD elements, fix to camera
+    hud = game.add.group();
+    hud.fixedToCamera = true;
+    var background = hud.create(0,0,'background');
+    var w = background.width;
+    var h = background.height;
+    radarMech = hud.create(w/2,h/2,'mech');
+    radarMech.x -= radarMech.width/2;
+    radarMech.y -= radarMech.height/2;
+
+    //Create radar blips
+    blip = game.add.sprite(80,40, 'blip');
+    game.physics.arcade.enable(blip);
+    var dx = blip.x - radarMech.x + radarMech.width/2;
+    var dy = blip.y - radarMech.y + radarMech.height/2;
+    blip.body.velocity.x = dx*dy/dx;
+    blip.body.velocity.y = dy*dx/dy;
+    blip.body.gravity.x = 0;
+    blip.body.gravity.y = 0;
+
+    //Create cursors
+    cursors = game.input.keyboard.createCursorKeys();
+  },
+  update: function(){
+    if (cursors.right.isDown){
+      blip.body.acceleration.x += -0.5;
+    }
+    else if (cursors.left.isDown){
+      blip.body.acceleration.x += 0.5;
+    }
+  }
+}
