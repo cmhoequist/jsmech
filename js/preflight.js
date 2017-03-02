5e10778ff461029e748a401ecd53960f1289dd5b
Moritz <cmhoequist@gmail.com>
2017-03-02 14:35:32 -0600

    Intermediate blip spawning commit

--- js/preflight.js
+++ js/preflight.js
@@ -1,4 +1,4 @@
-var hud, blip, radarMech, cursors;
+var hud, blip, blips, radarMech, cursors;
 
 var preflight = {
   //Member functions declared here
@@ -20,6 +20,16 @@ var preflight = {
     radarMech.y -= radarMech.height/2;
 
     //Create radar blips
+    blips = game.add.group();
+    for(var i = 0; i < 3; i++){
+      var randx = (hud.x + hud. Math.random() * (hud.x+hud.width - hud.x) + hud.x;
+      var randy = Math.sqrt(hud.width*hud.width/4-randx*randx);
+      blip = blips.create(randx,hud.x+hud.height/2 + randy,'blip');
+      // var dx = blip.x - radarMech.x + radarMech.width/2;
+      // var dy = blip.y - radarMech.y + radarMech.height/2;
+      // blip.body.velocity.x = dx*dy/dx;
+      // blip.body.velocity.y = dy*dx/dy;
+    }
     blip = game.add.sprite(80,40, 'blip');
     game.physics.arcade.enable(blip);
     var dx = blip.x - radarMech.x + radarMech.width/2;
@@ -31,11 +41,27 @@ var preflight = {
     cursors = game.input.keyboard.createCursorKeys();
   },
   update: function(){
+    this.checkRadarBounds();
     if (cursors.right.isDown){
-      blip.body.acceleration.x += -0.5;
+      blip.body.acceleration.x += -2;
     }
     else if (cursors.left.isDown){
-      blip.body.acceleration.x += 0.5;
+      blip.body.acceleration.x += 2;
+    }
+    else if(cursors.up.isDown){
+      blip.body.acceleration.y += 2;
+    }
+    else if(cursors.down.isDown){{
+      blip.body.acceleration.y -= 2;
+    }}
+  },
+  checkRadarBounds(){
+    var dx = (blip.x + blip.width/2) - (hud.x + hud.width/2);
+    var dy = (blip.y + blip.height/2) - (hud.y + hud.height/2);
+    var radius = hud.height/2;
+    var distance = Math.sqrt(dx*dx+dy*dy);
+    if(distance+blip.width >= radius){
+      blip.kill();
     }
   }
 }
